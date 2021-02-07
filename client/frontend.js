46348051d4c2f8cc743d369758c8f2651d345291
Nebado <ruslan.nbd@gmail.com>
2021-02-07 13:48:32 +0200

    Get value from form using Vue

--- client/frontend.js
+++ client/frontend.js
@@ -1,3 +1,21 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.esm.browser.js'
 
-console.log(Vue)
+new Vue({
+    el: '#app',
+    data() {
+        return {
+            form: {
+                name: '',
+                value: ''
+            }
+        }
+    },
+    methods: {
+        createContact() {
+            const {...contact} = this.form
+            console.log(contact)
+
+            this.form.name = this.form.value = ''
+        }
+    }
+})
